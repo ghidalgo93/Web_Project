5e36249ec17a66eed3dd5b784be9479c95737508
Gerardo Hidalgo <gerardo@pop-os.localdomain>
2020-06-24 14:33:07 -0600

    added icons, fixed side navbar, and added better resume

--- Website/static/functions.js
+++ Website/static/functions.js
@@ -6,6 +6,7 @@ function darkmode() {
     var linkedin = document.getElementById('linkedin-logo')
     var instagram = document.getElementById('instagram-logo')
     var twitter = document.getElementById('twitter-logo')
+    var gh = document.getElementById('gh-logo')
 
 
     if (body.className === "light-theme") {
@@ -16,14 +17,16 @@ function darkmode() {
         linkedin.src = "static/media/linkedin-dark.svg"
         instagram.src = "static/media/instagram-dark.svg"
         twitter.src = "static/media/twitter-dark.svg"
+        gh.src = "static/media/gh-dark.png"
     }
     else {
         body.className = "light-theme"
         nav.className = "navbar sticky-top navbar-expand navbar-light"
         button.innerHTML = "Dark Mode"
-        github.src = "/static/media/github.svg"
+        github.src = "static/media/github.svg"
         linkedin.src = "static/media/linkedin.svg"
         instagram.src = "static/media/instagram.svg"
         twitter.src = "static/media/twitter.svg"
+        gh.src = "static/media/gh.png"
     }
 }
